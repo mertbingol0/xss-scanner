82309fd88a06ec26f83a3bce471cc728885d5b3c
Mert Bingol <mertbingol00@protonmail.com>
2023-02-21 00:27:56 +0300

    Update xss-scanner.py

--- xss-scanner.py
+++ xss-scanner.py
@@ -4,68 +4,75 @@ Author: mertbingol0
 Date: 11/13/2022
 """
 
-from bs4 import BeautifulSoup as bs 
-from urllib.parse import urljoin
-import requests 
-
-
+# Define a function to retrieve all the forms present on the webpage
 def get_all_forms(url):
-    soup = bs(requests.get(url).content,"html.parser") #Burada tum htmli urlden istiyoruz.
-    return soup.find_all("form") #Bu kisimda da sadece "form" icerigini dondurmesini soyluyoruz.
-    
-#Bu fonksyonda ise form icersinde ki inputlardan tum degerleri alip detailsin icersine return ediyoruz.
+    soup = bs(requests.get(url).content, "html.parser")
+    return soup.find_all("form")
+
+# Define a function to retrieve all the details of a given form
 def get_forms_details(form):
     details = {}
-
-    action = form.attrs.get("action").lower() #Bu satirda form kisminda bir search islemi yapildiginda urldeki action degerini aliyoruz.
-    method = form.attrs.get("method","get").lower() #lower() fonksyonu stringleri kucultmeye yarar. Ornek: mErt = mert
-
-    inputs = [] 
-    for input_tag in form.find_all("input"): # Burada form icersinde ki input degerlerini aliyoruz.
-        input_type = input_tag.attrs.get("type","text") #Burada yukaridaki satirda yaptigimiz input alma isleminin icerisindeki type degerini aliyoruz. Fakat burada aldigimiz type degerinin "text" olacagini da soyluyoruz.
-        input_name = input_tag.attrs.get("name") #Burada da yine yukaridaki gibi input icerisinde ki name parametresinin degerini aliyoruz.
-        inputs.append({"type": input_type, "name":input_name}) #Burada for icinde aldigimiz tum degerleri, inputs dizisine veriyoruz.
-
-    details["action"] = action 
+    
+    # Extract the 'action' attribute of the form
+    action = form.attrs.get("action").lower()
+    
+    # Extract the 'method' attribute of the form
+    method = form.attrs.get("method", "get").lower()
+    
+    # Extract the 'name' and 'type' attributes of all input fields of the form
+    inputs = []
+    for input_tag in form.find_all("input"):
+        input_type = input_tag.attrs.get("type", "text")
+        input_name = input_tag.attrs.get("name")
+        inputs.append({"type": input_type, "name": input_name})
+    
+    # Store the form details in a dictionary
+    details["action"] = action
     details["method"] = method
-    details["inputs"] = inputs 
+    details["inputs"] = inputs
+    
+    return details
 
-    return details 
-#Bu fonksiyonda ise details fonksyonundaki inputlara payload islemi yapcagiz. Yani inputlara xss testimizi gerceklestirecegiz.
-def submit_forms(form_details,url,value):
-
-    target_url = urljoin(url,form_details["action"]) #Burada url'i ve action kismini verip target_url adi altinda bunlari toparlayacak.
-
-    inputs = form_details["inputs"] #form detailden vermis oldugumuz inputu inputs degiskenine atiyoruz.
-    data  = {}
-
-#inputlar
+# Define a function to submit a form with an XSS payload
+def submit_forms(form_details, url, value):
+    target_url = urljoin(url, form_details["action"])
+    inputs = form_details["inputs"]
+    data = {}
+    
+    # For each input field, set its value to the XSS payload if it is of type 'text' or 'search'
     for input in inputs:
-        if input["type"] == "text" or input["type"] == "search": #eger aldimiz input degerinin type'i text ise yada search ise bu kosula gir ve value ile xss payloadimizi input'a ver.
-            input["value"] = value 
+        if input["type"] == "text" or input["type"] == "search":
+            input["value"] = value
             input_name = input.get("name")
             input_value = input.get("value")
-            if input_name and input_value : # yukarida ki iki degerde olusturulduysa bu kosulu calistir.
-                data[input_name] = input_value 
-            if form_details["method"] == "post": # eger method degeri post ise, post metodu requesti at.
-                return requests.post(target_url,data=data)
-            else: # eger method degeri post degilse, get metodu ile request at.
-                return requests.get(target_url,params=data)
+            if input_name and input_value:
+                data[input_name] = input_value
+        
+        # If the form method is 'POST', make a POST request with the form data
+        if form_details["method"] == "post":
+            return requests.post(target_url, data=data)
+        # If the form method is not 'POST', make a GET request with the form data
+        else:
+            return requests.get(target_url, params=data)
 
-#Burada da alinan url uzerinde xss zafiyeti icin bir payload islemi yapiliyor.
-def xss_scanner (url):
+# Define a function to scan a webpage for XSS vulnerabilities
+def xss_scanner(url):
     forms = get_all_forms(url)
     print("Searching for XSS vulnerability...")
     xss_payload = "<script>alert('xss-test')</script>"
-    is_vuln = False 
-    for form in forms: #bazi sitelerde birden fazla form bolumu olabiliyor. Bu nedenle tum formlari inceleyecek bir dongu yazdik.
+    is_vuln = False
+    
+    # For each form on the webpage, submit it with an XSS payload and check if the payload is present in the response
+    for form in forms:
         form_details = get_forms_details(form)
-        content = submit_forms(form_details,url,xss_payload).content.decode()
-        if  xss_payload in content: #Eger xss_payload degerimiz bize donen response icerisinde varsa zafiyet vardir.
+        content = submit_forms(form_details, url, xss_payload).content.decode()
+        if xss_payload in content:
             print("XSS vulnerability detected!")
             is_vuln = True
-    return is_vuln 
+    
+    return is_vuln
 
+# If the script is run from the command line, prompt the user for a URL to scan and call the 'xss_scanner' function
 if __name__ == "__main__":
     url = input("Enter site address for XSS search: ")
     is_vulne = xss_scanner(url)
